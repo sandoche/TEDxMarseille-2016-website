b5233a5b720be65365d6e9d8cc5360a8b47d3136
Sandoche Adittane <sandoche.adittane@nl.ibm.com>
2016-06-08 22:32:00 +0200

    titles style updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,12 +19,12 @@ $('#navbarSpy').on('activate.bs.scrollspy', function () {
   if(currentItem < 3) {
     $('.zone').removeClass('color-2');
     $('.zone').addClass('color-1');
-    $('h2').removeClass('color-2-text');
-    $('h2').addClass('color-1-text');
+    $('.background-text').removeClass('color-2-text');
+    $('.background-text').addClass('color-1-text');
   } else {
     $('.zone').removeClass('color-1');
     $('.zone').addClass('color-2');
-    $('h2').removeClass('color-1-text');
-    $('h2').addClass('color-2-text');
+    $('.background-text').removeClass('color-1-text');
+    $('.background-text').addClass('color-2-text');
   }
 })
